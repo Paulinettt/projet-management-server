a4582822fc56da48059717d9b75337bcc8a8b4aa
Pauline Schnoebelen <pauline.schnoebelen@gmail.com>
2022-05-17 16:18:06 +0200

    add routes

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const Project = require('../models/Project.model');
-// const Task = require('../models/Task.model');
+const Task = require('../models/Task.model');
 
 
 // Create new project
@@ -16,7 +15,7 @@ router.post('/projects', (req, res, next) => {
         tasks: []
     }
 
-    Project.create(newProject)
+    Project.create(newProject) //send a query to the DB 
         .then(response => res.status(201).json(response))
         .catch(err => {
             console.log("error creating a new project", err);
@@ -27,4 +26,98 @@ router.post('/projects', (req, res, next) => {
         })
 });
 
+// List all projects
+router.get("/projects", (req, res, next) => {
+    Project.find()
+        .populate("tasks")
+        .then( response => {
+            res.json(response)
+        })
+        .catch(err => {
+            console.log("error getting list of projects", err);
+            res.status(500).json({
+                message: "error getting list of projects",
+                error: err
+            });
+        })
+});
+
+//  Get details of a specific project by id
+router.get('/projects/:projectId', (req, res, next) => {
+    
+    const { projectId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    Project.findById(projectId)
+        .populate('tasks')
+        .then(project => res.json(project))
+        .catch(err => {
+            console.log("error getting details of a project", err);
+            res.status(500).json({
+                message: "error getting details of a project",
+                error: err
+            });
+        })
+});
+
+// PUT  /api/projects/:projectId  -  Updates a specific project by id
+router.put('/projects/:projectId', (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    // const newDetails = {
+    //     title: req.body.title,
+    //     description: req.body.description,
+    //     tasks: req.body.tasks
+    // }
+
+    Project.findByIdAndUpdate(projectId, req.body, { new: true })
+        .then((updatedProject) => res.json(updatedProject))
+        .catch(err => {
+            console.log("error updating project", err);
+            res.status(500).json({
+                message: "error updating project",
+                error: err
+            });
+        })
+});
+
+// Delete a specific project by id
+router.delete('/projects/:projectId', (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    // Project.findByIdAndRemove(projectId)
+    //     .then(() => res.json({ message: `Project with ${projectId} is removed successfully.` }))
+    Project.findByIdAndRemove(projectId)
+        .then(deteletedProject => {
+            return Task.deleteMany({ _id: { $in: deteletedProject.tasks } });
+        })
+        .then(() => res.json({ message: `Project with id ${projectId} & all associated tasks were removed successfully.` }))
+        .catch(err => {
+            console.log("error deleting project", err);
+            res.status(500).json({
+                message: "error deleting project",
+                error: err
+            });
+        })
+});
+
+
+
+
 module.exports = router;
+
+
